99fb4d5a1b9e1b8bc8592454a2c8b44d311f470e
nikosszzz <19nikospap@gmail.com>
2023-03-25 19:06:35 +0200

    friendInvites: finalize

--- plugins/friendInvites/src/index.ts
+++ plugins/friendInvites/src/index.ts
@@ -2,72 +2,78 @@ import { logger } from "@vendetta";
 import { registerCommand } from "@vendetta/commands";
 import { findByProps } from "@vendetta/metro";
 
+let commands = [];
+
 export default {
     onLoad: () => {
-        let commands = [];
-        const ClydeUtils = findByProps("sendBotMessage");
-        const inviteModule = findByProps("createFriendInvite");
+        try {
+            const ClydeUtils = findByProps("sendBotMessage");
+            const inviteModule = findByProps("getAllFriendInvites");
 
-        commands.push(registerCommand({
-            name: "invitecreate",
-            displayName: "invitecreate",
-            description: "Generates a friend invite link.",
-            displayDescription: "Generates a friend invite link.",
-            type: 1,
-            //@ts-ignore
-            applicationId: -1,
-            inputType: 1,
-            execute: async (_, ctx) => {
-                const createInvite = await inviteModule.createFriendInvite();
-                const message = `
-                        discord.gg/${createInvite.code} ·
+            commands.push(registerCommand({
+                name: "invite create",
+                displayName: "invite create",
+                description: "Generates a friend invite link.",
+                displayDescription: "Generates a friend invite link.",
+                type: 1,
+                //@ts-ignore
+                applicationId: -1,
+                inputType: 1,
+                execute: async (_, ctx) => {
+                    const createInvite = await inviteModule.createFriendInvite();
+                    const message = `
+                        https://discord.gg/${createInvite.code} ·
                         Expires: <t:${new Date(createInvite.expires_at).getTime() / 1000}:R> ·
                         Max uses: \`${createInvite.max_uses}\`
                     `.trim().replace(/\s+/g, " ")
-    
-                ClydeUtils.sendBotMessage(ctx.channel.id, message);
-            }
-        }));
 
-        commands.push(registerCommand({
-            name: "viewinvites",
-            displayName: "viewinvites",
-            description: "View your current friend invite links that you've made.",
-            displayDescription: "View your current friend invite links that you've made.",
-            type: 1,
-            //@ts-ignore
-            applicationId: -1,
-            execute: async (_, ctx) => {
-                const invites = await inviteModule.getAllFriendInvites();
-                const friendInviteList = invites.map(i =>
-                    `_discord.gg/${i.code}_ ·
+                    ClydeUtils.sendBotMessage(ctx.channel.id, message);
+                }
+            }));
+
+            commands.push(registerCommand({
+                name: "view invites",
+                displayName: "view invites",
+                description: "View your current friend invite links that you've made.",
+                displayDescription: "View your current friend invite links that you've made.",
+                type: 1,
+                //@ts-ignore
+                applicationId: -1,
+                execute: async (_, ctx) => {
+                    const invites = await inviteModule.getAllFriendInvites();
+                    const friendInviteList = invites.map(i =>
+                        `_https://discord.gg/${i.code}_ ·
                     Expires: <t:${new Date(i.expires_at).getTime() / 1000}:R> ·
                     Times used: \`${i.uses}/${i.max_uses}\``.trim().replace(/\s+/g, " ")
-                );
+                    );
 
-                ClydeUtils.sendBotMessage(ctx.channel.id, friendInviteList.join("\n") || "You have no active friend invites!");
-            }
-        }));
+                    ClydeUtils.sendBotMessage(ctx.channel.id, friendInviteList.join("\n") || "You have no active friend invites!");
+                }
+            }));
 
-        commands.push(registerCommand({
-            name: "revokeinvites",
-            displayName: "revokeinvites",
-            description: "Revoke all your friend invite links.",
-            displayDescription: "Revoke all your friend invite links.",
-            type: 1,
-            //@ts-ignore
-            applicationId: -1,
-            inputType: 1,
-            execute: async (_, ctx) => {
-                await findByProps("createFriendInvite").revokeFriendInvites();
+            commands.push(registerCommand({
+                name: "revoke invites",
+                displayName: "revoke invites",
+                description: "Revoke all your friend invite links.",
+                displayDescription: "Revoke all your friend invite links.",
+                type: 1,
+                //@ts-ignore
+                applicationId: -1,
+                inputType: 1,
+                execute: async (_, ctx) => {
+                    await inviteModule.revokeFriendInvites();
 
-                ClydeUtils.sendBotMessage(ctx.channel.id, "All friend invites have been revoked.");
-            }
-        }));
+                    ClydeUtils.sendBotMessage(ctx.channel.id, "All friend invites have been revoked.");
+                }
+            }));
 
-        logger.log("FriendInvites has been initialized.");
+            logger.log("FriendInvites has been initialized.");
+        } catch (e) {
+            logger.error(e.stack);
+        }
     },
     onUnload: () => {
         logger.log("FriendInvites has been stopped.");
+        for (const unregisterCommands of commands) unregisterCommands();
     }
 }
